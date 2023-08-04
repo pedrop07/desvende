67c7eea1001ec031aa10bcb20e84fae235a491f4
Pedro <pedrofulfaro@gmail.com>
2023-08-04 20:36:41 -0300

    fix: adjust DataBase

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,36 +1,36 @@
-import { NextResponse } from "next/server"
-import { dictionary } from "../../../../dictionary";
-import { prisma } from "@/lib/prisma";
+// import { NextResponse } from "next/server"
+// import { dictionary } from "../../../../dictionary";
+// import { prisma } from "@/lib/prisma";
 
-export async function GET() {
-  try {
-    const answer = dictionary[Math.floor(Math.random() * 867)].toUpperCase()
+// export async function GET() {
+//   try {
+//     const answer = dictionary[Math.floor(Math.random() * 867)].toUpperCase()
 
-    let existAnswer = await prisma.answer.findUnique({
-      where: {
-        id: process.env.ANSWER_ID,
-      },
-    })
+//     let existAnswer = await prisma.answer.findUnique({
+//       where: {
+//         id: process.env.ANSWER_ID,
+//       },
+//     })
 
-    if(existAnswer){
-      await prisma.answer.update({
-        where: {
-          id: process.env.ANSWER_ID
-        },
-        data: {
-          answer
-        }
-      })
-    } else {
-      await prisma.answer.create({
-        data: {
-          answer
-        }
-      })
-    }
+//     if(existAnswer){
+//       await prisma.answer.update({
+//         where: {
+//           id: process.env.ANSWER_ID
+//         },
+//         data: {
+//           answer
+//         }
+//       })
+//     } else {
+//       await prisma.answer.create({
+//         data: {
+//           answer
+//         }
+//       })
+//     }
 
-    return NextResponse.json({ answer });
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+//     return NextResponse.json({ answer });
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }

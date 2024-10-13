1187e8f8862df54d5221a94424f831ba173c1760
Pedro <pedrofulfaro@gmail.com>
2024-10-13 16:50:47 -0300

    chore: remove database and prisma config

--- prisma/migrations/20230804192012_create_db/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Answer" (
-    "id" TEXT NOT NULL PRIMARY KEY DEFAULT '3a41c5dc-bc10-4e79-87f4-6c3d3845d794',
-    "answer" TEXT NOT NULL
-);
-
--- Insert Value
-INSERT INTO "Answer" (answer) VALUES ('NAVIO');
--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,21 +1,17 @@
-import { getAnswer } from '@/actions/get-answer';
 import { Header } from '@/components/header';
 import { Rows } from '@/components/rows';
+import { possibleAnswers } from '../../../constants/possible-answers';
 
 export default async function Home() {
-  const { answer } = await getAnswer();
+  const answer = possibleAnswers[Math.floor(Math.random() * 914)].toUpperCase()
 
-  const answerString = answer;
+  const answerString = answer
   const answerArray = answer?.split('');
 
   return (
     <div>
       <Header />
-
-      <Rows
-        answerArray={answerArray}
-        answerString={answerString}
-      />
+      <Rows answerArray={answerArray} answerString={answerString} />
     </div>
   )
 }
